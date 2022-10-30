31a8bc0438444699284c409b54e331d21d0743ea
alukic <alukic@span.eu>
2022-10-30 23:02:36 +0100

    commit message

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {TableComponent} from "./module/lab1/table/table.component";
 
 export const routes: Routes = [
 
 
-  {path: '', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)}
+  {path: '',component:TableComponent}
 
 
 ];
