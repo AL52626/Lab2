d3d3e380f09f09a9274e4202a90412c6c8fb2004
alukic <alukic@span.eu>
2022-10-30 22:25:10 +0100

    commit message

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,11 +3,9 @@ import {RouterModule, Routes} from '@angular/router';
 
 export const routes: Routes = [
 
-  {path: '', redirectTo: 'home'},
-  {
-    path: '', children: [
-      {path: 'home', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)}]
-  },
 
-];
+    {path: '', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)}
+
+  ]
+;
 
