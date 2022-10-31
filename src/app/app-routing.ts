d1feac3a0840d13b84b23157e5bfadaad996b652
alukic <alukic@span.eu>
2022-10-31 16:09:01 +0100

    ss

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -4,9 +4,10 @@ import {RouterModule, Routes} from '@angular/router';
 export const routes: Routes = [
 
 
-  {path: "**", loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)},
-  //{path: '**', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)},
-
+  {path: '', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)},
+  {path: '/', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)},
+  {path: '*', loadChildren: () => import('./module/lab1/table/table.module').then(m => m.TableModule)},
+  {path: '',redirectTo:'Lab1/nogomet'},
 
 
 ];
