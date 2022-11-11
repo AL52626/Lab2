56d73a0960c4049b561ab019b64de7b5bbd513ce
alukic <alukic@span.eu>
2022-11-11 19:34:11 +0100

    sad

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -9,14 +9,10 @@ export const routes: Routes = [
     loadChildren: () => import('./module/lab2/attack_test/attack_test.module').then(m => m.Attack_testModule)
   },
   {
-    path: '/',
-    loadChildren: () => import('./module/lab2/attack_test/attack_test.module').then(m => m.Attack_testModule)
-  },
-  {
     path: '**',
     loadChildren: () => import('./module/lab2/attack_test/attack_test.module').then(m => m.Attack_testModule)
   },
-  {path: '', redirectTo: 'Lab2'},
+  {path: '', redirectTo: 'Lab2',pathMatch:"full"},
 
 
 ];
